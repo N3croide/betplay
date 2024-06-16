b6f0a2c95e9c6310661b7860541b054fa90069d7
N3croide <106851453+N3croide@users.noreply.github.com>
2024-06-16 16:06:20 -0500

    added some attributes to match

--- src/main/java/com/aeb/modules/match/domain/Match.java
+++ src/main/java/com/aeb/modules/match/domain/Match.java
@@ -1,5 +1,16 @@
 package com.aeb.modules.match.domain;
 
+import java.sql.Date;
+
+import com.aeb.modules.team.domain.Team;
+
 public class Match {
+
+    /* private Tuple */
+    private Team local;
+    private Team visitor;
+    private Integer localGoals;
+    private Integer visitorGoals;
+    private Date date;
 
 }
